6e0eb6934927b5551aeef9299cc5fa736d603848
erickcsh93 <erickcsh@gmail.com>
2013-12-09 13:39:46 -0600

    Fully implemented

--- Start.rb
+++ Start.rb
@@ -2,24 +2,53 @@ class Start
 
   def initialize
     @players_number = 1
+    @players = []
   end
 
   def start
-    #player = Player.new("a",false)
     Instructions.display_beginning_instructions
     ask_for_mode
+    enter_players_name
   end
 
   def ask_for_mode
     puts "Select a mode:
            1) 1 player
            2) 2 players"
-   mode = STDIN.gets.chomp
-   check_mode(mode)
+   enter_mode
+   start_game
   end
 
-  def check_mode(mode)
+  def enter_mode
+    mode = STDIN.gets.chomp
+    while !mode_is_valid? (mode.to_i)
+      puts "Enter a valid mode"
+      mode = STDIN.gets.chomp
+    end
+    assign_player_mode(mode.to_i)
+  end
+
+  def mode_is_valid?(mode)
+    mode == 1 || mode == 2
+  end
+
+  def assign_player_mode(mode)
     @players_number = mode
+  end
+
+  def enter_players_name
+    @players_number.times { |count| @players[count] = Player.new(ask_for_name(count), false) }
+    @players[1] = Player.new("Computer", true) if @players_number == 1
+  end
+
+  def ask_for_name(player_number)
+    puts "Enter player #{player_number + 1} name"
+    STDIN.gets.chomp
+  end
+
+  def start_game
+    system "clear"
+    Gameplay.new.play
   end
 end
 
