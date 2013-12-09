d425d14088d6e7179e25b944ae1abd37975f337c
erickcsh93 <erickcsh@gmail.com>
2013-12-09 09:22:43 -0600

    Instructions first definition

---
+++ Instructions.rb
@@ -0,0 +1,13 @@
+module Instructions
+
+  INSTRUCTIONS_AT_BEGINNING = ""
+  INSTRUCTIONS_AT_GAMEPLAY = ""
+
+  def self.display_beginning_instructions
+    puts INSTRUCTIONS_AT_BEGINNING
+  end
+
+  def self.display_gameplay_instructions
+    puts INSTRUCTIONS:_AT_GAMEPLAY
+  end
+end
