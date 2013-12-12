fb120250f6ab68346626db747867ce465bd097d2
erickcsh93 <erickcsh@gmail.com>
2013-12-12 10:48:03 -0600

    Reorganize files

---
+++ tic_tac_toe/board.rb
@@ -0,0 +1,98 @@
+module TicTacToe
+  class Board
+		
+  	DICTIONARY_NUMBER_SYMBOL = {0 => " ", 1 => "X", 2 => "O"}
+  	DICTIONARY_INDEX_LETTER = {0 => 'A', 1 => 'B', 2 => 'C'}
+  	EMPTY = 0
+	
+  	def initialize
+  		create_board
+  	end
+	
+  	def create_board
+  		@board = [[EMPTY,EMPTY,EMPTY],[EMPTY,EMPTY,EMPTY],[EMPTY,EMPTY,EMPTY]]
+  	end
+	
+	  def rows
+	    @board 
+	  end
+		
+  	def fill_board_space(position, player_number)
+  		position = GridPosition.new(position)
+  		@board[position.row][position.col] = player_number
+  	end
+	
+  	def is_position_empty?(position)
+  		position = GridPosition.new(position)
+  		@board[position.row][position.col] == EMPTY
+  	end
+	
+  	def get_empty_postions
+  		@board.each_with_index.reduce([]) { |positions, row_with_index| positions << get_col_empty_positions(row_with_index.first, row_with_index.last) }.flatten!
+  	end
+	
+  	def get_col_empty_positions(row, index)
+  		letter = index_to_letter(index)
+  		row.each_with_index.reduce([]) do |positions, element_with_index|  
+  			letter = index_to_letter(element_with_index.last)
+  			element_with_index.first == EMPTY ?  positions << "#{letter},#{index + 1}" : positions
+  		end
+  	end
+	
+	
+  	def index_to_letter(index)
+  		DICTIONARY_INDEX_LETTER[index]
+  	end
+	
+  	def draw?
+  		draw = true
+  		@board.each {|row| row.each { |element| draw = false if element == EMPTY}}
+  		draw
+  	end
+	
+  	def win? (player_number)
+  		vertical_win?(player_number) || horizontal_win?(player_number) || diagonal_win?(player_number)
+  	end
+	
+  	def vertical_win?(player_number)
+  		win = false
+  		3.times { |count| win = true if win_vertical_line?(player_number, count) }
+  		win
+  	end
+	
+  	def win_vertical_line?(player_number, col)
+  		@board[0][col] == player_number && 
+  		@board[1][col] == player_number &&
+  		@board[2][col] == player_number
+  	end
+	
+  	def horizontal_win?(player_number)
+  		win = false
+  		3.times { |count| win = true if win_horizontal_line?(player_number,count)}
+  		win
+  	end
+	
+  	def win_horizontal_line?(player_number, row)
+  		@board[row][0] == player_number &&
+  		@board[row][1] == player_number &&
+  		@board[row][2] == player_number
+  	end
+	
+  	def diagonal_win?(player_number)
+  		left_to_right_diagonal_win?(player_number) || right_to_left_diagonal_win?(player_number)
+  	end
+	
+  	def left_to_right_diagonal_win?(player_number)
+  		@board[0][0] == player_number &&
+  		@board[1][1] == player_number &&
+  		@board[2][2] == player_number
+  	end
+	
+  	def right_to_left_diagonal_win?(player_number)
+  		@board[0][2] == player_number &&
+  		@board[1][1] == player_number &&
+  		@board[2][0] == player_number
+  	end
+	
+  end
+end
---
+++ tic_tac_toe/start.rb
@@ -0,0 +1,74 @@
+module TicTacToe  
+  class Start
+
+    def initialize
+      @players_number = 1
+      @players = []
+    end
+
+    def start
+      Instructions.display_beginning_instructions
+      ask_for_mode
+      enter_players_name
+      play_until_no_more_replay
+    end
+
+    def play_until_no_more_replay
+      replay = true
+      while replay
+        start_game
+        replay = ask_for_replay
+      end
+    end
+
+    def ask_for_replay
+      puts "Do you want to play again [Y/N]"
+      replay = STDIN.gets.chomp
+      wants_to_replay?(replay)
+    end
+
+    def wants_to_replay?(replay)
+      replay.downcase.eql?('y')
+    end
+
+    def ask_for_mode
+      puts "Select a mode:
+             1) 1 player
+             2) 2 players"
+     enter_mode
+    end
+
+    def enter_mode
+      mode = STDIN.gets.chomp
+      while !mode_is_valid? (mode.to_i)
+        puts "Enter a valid mode"
+        mode = STDIN.gets.chomp
+      end
+      assign_player_mode(mode.to_i)
+    end
+
+    def mode_is_valid?(mode)
+      mode == 1 || mode == 2
+    end
+
+    def assign_player_mode(mode)
+      @players_number = mode
+    end
+
+    def enter_players_name
+      @players_number.times { |count| @players[count] = Player.new(ask_for_name(count), false) }
+      @players[1] = Player.new("Computer", true) if @players_number == 1
+    end
+
+    def ask_for_name(player_number)
+      puts "Enter player #{player_number + 1} name"
+      STDIN.gets.chomp
+    end
+
+    def start_game
+      system "clear"
+      Gameplay.new(@players).play
+    end
+  end
+end
+
