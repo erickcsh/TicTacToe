5496bbbf0c1239d4dcc168b69bc2409745f44e66
erick <erick@erick-Inspiron-N7010.(none)>
2014-01-17 11:08:30 -0600

    GridPosition tested with rspec

--- lib/tic_tac_toe/grid_position.rb
+++ lib/tic_tac_toe/grid_position.rb
@@ -5,16 +5,12 @@ module TicTacToe
 
     attr_accessor :row, :col
 
-    def initialize(specs)
-      @row = specs[:row]
-      @col = specs[:col]
+    def initialize(args)
+      @row = args[:row]
+      @col = args[:col]
     end
 
     def self.from_string(string_position)
-      position_from_string(string_position)
-    end
-
-    def self.position_from_string(string_position)
       col, row = string_position.split(',')
       row = row.to_i - 1
       col = DICTIONARY_LETTER_NUMBER_TRANSFORMATION[col]
